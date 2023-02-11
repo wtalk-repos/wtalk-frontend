52b97b8da72ef212aede78d78cb238c40d38e4a1
Bojan Balinovic <bojanbalinovic1@gmail.com>
2023-02-11 22:15:05 +0100

    fix left/right messages

--- src/app/modules/chat/models/message.ts
+++ src/app/modules/chat/models/message.ts
@@ -5,7 +5,9 @@ export class Message {
     text: string;
     receiver: Friend;
     receiverId: number;
-
+    sender:Friend;
+    senderId:number;
+    
     constructor(props?: Message) {
         if (props) {
             Object.assign(this, props);
